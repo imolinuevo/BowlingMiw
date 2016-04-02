401bf89369f5b4c6fbeb2c375395ba4c58671667
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2016-04-02 16:15:23 +0200

    Ad fields can't be null implementation with test.

--- src/main/java/miw/mwl/BowlingMiw/Advertisement.java
+++ src/main/java/miw/mwl/BowlingMiw/Advertisement.java
@@ -1,15 +1,32 @@
 package miw.mwl.BowlingMiw;
 
+import miw.mwl.BowlingMiw.Exceptions.MalformedAdvertisementException;
+
 public class Advertisement {
 
 	private String company;
 	private String slogan;
 	private String description;
 
-	public Advertisement(String company, String slogan, String description) {
-		this.company = company;
-		this.slogan = slogan;
-		this.description = description;
+	public Advertisement(String company, String slogan, String description)
+			throws MalformedAdvertisementException {
+		if (isMalformed(company, slogan, description)) {
+			throw new MalformedAdvertisementException();
+		} else {
+			this.company = company;
+			this.slogan = slogan;
+			this.description = description;
+		}
+	}
+
+	private boolean isMalformed(String company, String slogan,
+			String description) {
+		if (company.trim().equals("") || slogan.trim().equals("")
+				|| description.trim().equals("")) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public String getCompany() {
--- src/test/java/miw/mwl/BowlingMiw/AdManagerTest.java
+++ src/test/java/miw/mwl/BowlingMiw/AdManagerTest.java
@@ -2,13 +2,15 @@ package miw.mwl.BowlingMiw;
 
 import static org.junit.Assert.*;
 import miw.mwl.BowlingMiw.Exceptions.AdAlreadyExistException;
+import miw.mwl.BowlingMiw.Exceptions.MalformedAdvertisementException;
 
 import org.junit.Test;
 
 public class AdManagerTest {
 
 	@Test
-	public void testUniqueAdsSuccess() throws AdAlreadyExistException {
+	public void testUniqueAdsSuccess() throws AdAlreadyExistException,
+			MalformedAdvertisementException {
 		AdManager adManager = AdManager.getInstance();
 		adManager.add(new Advertisement("Sony", "Make belive",
 				"Legal description"));
@@ -18,12 +20,19 @@ public class AdManagerTest {
 	}
 
 	@Test(expected = AdAlreadyExistException.class)
-	public void testUniqueAdsError() throws AdAlreadyExistException {
+	public void testUniqueAdsError() throws AdAlreadyExistException,
+			MalformedAdvertisementException {
 		AdManager adManager = AdManager.getInstance();
 		adManager.add(new Advertisement("Sony", "Make belive",
 				"Legal description"));
 		adManager.add(new Advertisement("Sony", "Make belive",
 				"Legal description"));
+	}
+
+	@Test(expected = MalformedAdvertisementException.class)
+	public void testAdvertisementWithEmptyFields()
+			throws MalformedAdvertisementException {
+		new Advertisement("", "", "");
 	}
 
 }
