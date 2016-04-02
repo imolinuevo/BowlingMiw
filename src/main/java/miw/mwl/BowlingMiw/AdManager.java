b006a309f9da6749b0d9a7fa01637792f4645baa
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2016-04-02 17:42:04 +0200

    Delete ad implementation with test.

--- src/main/java/miw/mwl/BowlingMiw/AdManager.java
+++ src/main/java/miw/mwl/BowlingMiw/AdManager.java
@@ -3,6 +3,8 @@ package miw.mwl.BowlingMiw;
 import java.util.ArrayList;
 
 import miw.mwl.BowlingMiw.Exceptions.AdAlreadyExistException;
+import miw.mwl.BowlingMiw.Exceptions.AdvertisementNotFoundException;
+import miw.mwl.BowlingMiw.Exceptions.MalformedAdvertisementException;
 
 public class AdManager {
 
@@ -25,20 +27,39 @@ public class AdManager {
 		return adList;
 	}
 
-	public void add(Advertisement advertisement) throws AdAlreadyExistException {
-		if (isUniqueAd(advertisement)) {
+	public void addAd(Advertisement advertisement)
+			throws AdAlreadyExistException {
+		if (!containsAd(advertisement)) {
 			adList.add(advertisement);
 		} else {
 			throw new AdAlreadyExistException();
 		}
 	}
 
-	private boolean isUniqueAd(Advertisement advertisement) {
+	private boolean containsAd(Advertisement advertisement) {
 		for (Advertisement ad : adList) {
 			if (ad.equals(advertisement)) {
-				return false;
+				return true;
 			}
 		}
-		return true;
+		return false;
+	}
+
+	public void deleteAd(String company, String slogan)
+			throws AdvertisementNotFoundException,
+			MalformedAdvertisementException {
+		Advertisement advertisement = findAdvertisement(company, slogan);
+		adList.remove(advertisement);
+	}
+
+	private Advertisement findAdvertisement(String company, String slogan)
+			throws AdvertisementNotFoundException {
+		for (Advertisement advertisement : adList) {
+			if (advertisement.getCompany().equals(company)
+					&& advertisement.getSlogan().equals(slogan)) {
+				return advertisement;
+			}
+		}
+		throw new AdvertisementNotFoundException();
 	}
 }
--- src/test/java/miw/mwl/BowlingMiw/AdManagerTest.java
+++ src/test/java/miw/mwl/BowlingMiw/AdManagerTest.java
@@ -2,6 +2,7 @@ package miw.mwl.BowlingMiw;
 
 import static org.junit.Assert.*;
 import miw.mwl.BowlingMiw.Exceptions.AdAlreadyExistException;
+import miw.mwl.BowlingMiw.Exceptions.AdvertisementNotFoundException;
 import miw.mwl.BowlingMiw.Exceptions.MalformedAdvertisementException;
 
 import org.junit.Test;
@@ -12,9 +13,9 @@ public class AdManagerTest {
 	public void testUniqueAdsSuccess() throws AdAlreadyExistException,
 			MalformedAdvertisementException {
 		AdManager adManager = AdManager.getInstance();
-		adManager.add(new Advertisement("Sony", "Make belive",
+		adManager.addAd(new Advertisement("Sony", "Make belive",
 				"Legal description"));
-		adManager.add(new Advertisement("Adidas", "Impossible is nothing",
+		adManager.addAd(new Advertisement("Adidas", "Impossible is nothing",
 				"Legal description"));
 		assertEquals(2, adManager.getAdList().size());
 	}
@@ -23,9 +24,9 @@ public class AdManagerTest {
 	public void testUniqueAdsError() throws AdAlreadyExistException,
 			MalformedAdvertisementException {
 		AdManager adManager = AdManager.getInstance();
-		adManager.add(new Advertisement("Sony", "Make belive",
+		adManager.addAd(new Advertisement("Sony", "Make belive",
 				"Legal description"));
-		adManager.add(new Advertisement("Sony", "Make belive",
+		adManager.addAd(new Advertisement("Sony", "Make belive",
 				"Legal description"));
 	}
 
@@ -33,6 +34,22 @@ public class AdManagerTest {
 	public void testAdvertisementWithEmptyFields()
 			throws MalformedAdvertisementException {
 		new Advertisement("", "", "");
+	}
+
+	@Test
+	public void testDeleteAdSuccess() throws AdAlreadyExistException,
+			MalformedAdvertisementException, AdvertisementNotFoundException {
+		AdManager adManager = AdManager.getInstance();
+		adManager.addAd(new Advertisement("Sony", "Make belive",
+				"Legal description"));
+		adManager.deleteAd("Sony", "Make belive");
+	}
+
+	@Test(expected = AdvertisementNotFoundException.class)
+	public void testDeleteAdError() throws AdvertisementNotFoundException,
+			MalformedAdvertisementException {
+		AdManager adManager = AdManager.getInstance();
+		adManager.deleteAd("Sony", "Make belive");
 	}
 
 }
