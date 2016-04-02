0c0e508a050b9fd535ab970dec357f383598bd0a
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2016-04-02 17:51:26 +0200

    Refactor: unused methods.

--- src/main/java/miw/mwl/BowlingMiw/Advertisement.java
+++ src/main/java/miw/mwl/BowlingMiw/Advertisement.java
@@ -33,24 +33,12 @@ public class Advertisement {
 		return company;
 	}
 
-	public void setCompany(String company) {
-		this.company = company;
-	}
-
 	public String getSlogan() {
 		return slogan;
 	}
 
-	public void setSlogan(String slogan) {
-		this.slogan = slogan;
-	}
-
 	public String getDescription() {
 		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
 	}
 
 	public boolean equals(Advertisement advertisment) {
